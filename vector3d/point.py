b7f894dafa5375b8f8300b61a97007a540cdb0ae
Ron Jensen <ron@jentronics.com>
2021-03-21 17:50:56 -0600

    Update git repository to what installs from PyPi as 1.1.1

--- vector3d/point.py
+++ vector3d/point.py
@@ -10,7 +10,7 @@ class Point:
 		self.z = z
 
 def distance(a, b):
-	return sqrt((b.x - a.x)^2 + (b.y - a.y)^2 + (b.z - a.z)^2)
+	return sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2) + pow((b.z - a.z), 2))
 
 def center(a, b):
 	return Point((b.x - a.x)/2, (b.y - a.y)/2, (b.z - a.z)/2)
--- vector3d/vector.py
+++ vector3d/vector.py
@@ -25,7 +25,8 @@ class Vector:
 		return Vector((self.x - o.x), (self.y - o.y), (self.z - o.z))
 	
 	def __mul__(self, o):
-		return ((self.x * o.x) + (self.y * o.y) + (self.z * o.z))
+		if type(o) is Vector: return ((self.x * o.x) + (self.y * o.y) + (self.z * o.z))
+		else: return Vector(self.x * o, self.y * o, self.z * o)
 	
 	def __iadd__(self, o):
 		self.x += o.x
@@ -43,7 +44,7 @@ class Vector:
 		return Vector(-self.x, -self.y, -self.z)
 	
 	def length(self):
-		return sqrt((self.x*self.x) + (self.y*self.y) + (self.z*self.z))
+		return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
 	
 	def normalize(self):
 		return Vector((self.x / self.length()), (self.y / self.length()), (self.z / self.length()))
@@ -53,10 +54,15 @@ def from_points(a=Point(0,0,0), b=Point(0,0,0)):
 
 def angle(a, b):
 	m = a.x*b.x + a.y*b.y + a.z*b.z
-	return degrees(acos(m / (a.length() * b.length())))
+	return degrees(acos(__fix_cosinus__(m / (a.length() * b.length()))))
 
 def horizontal_angle(a, b):
 	return angle(Vector(a.x, a.y, 0), Vector(b.x, b.y, 0))
 
 def vertical_angle(a, b):
 	return angle(Vector(0, a.y, a.z), Vector(0, b.y, b.z))
+
+def __fix_cosinus__(a):
+	if a > 1: return 1
+	if a < -1: return -1
+	else: return a
